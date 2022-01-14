b820eb5ccac05013004b8860f6a96d34cce58fd9
Scott Donnan <scott.donnan@me.com>
2022-01-14 11:20:19 -0600

    testing for video

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,8 +11,8 @@ function About() {
                     <h1>Scott Donnan</h1>
                     <p>Full Stack Developer | MBA</p>
                 </div>
-                <img src={scott}/>
                 <div>
+                    <img src={scott}/>
                     <h3>Summary</h3>
                     <p>Hi I'm Scott Donnan, a full stack developer with a diverse professional background.  I've spent over 10 years in business management, hospitality, retail, and music performance.  I have experience working with Ruby on Rails, JavaScript, and React.  My passion is creating new programs and solutions centered on improving customer experiences through technology.  I am dedicated to continuous growth, development, and sustainability.</p>
                     <p>If you're interested in working together or just want to connect, give me a shout.  My info is down at the bottom!</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import '../App.css'
+import demo from '../videos/RuneTradeDemo.mp4'
 
 function Projects() {
     return(
@@ -7,6 +8,9 @@ function Projects() {
                 <h1>Projects</h1>
                 <div>
                     <a href="https://runetrade.herokuapp.com/">RuneTrade</a>
+                    <video width="500" controls>
+                        <source src={demo} type="video/mp4"/>
+                    </video>
                 </div>
                 <div>
                     <a>Plant Project</a>
