be98c8c70547a731550b2810621b32c49a8ccb9e
Scott Donnan <scott.donnan@me.com>
2022-01-21 15:42:37 -0600

    started on contact section

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,10 +8,16 @@ function Links() {
         <div ref={links} className="main-section">
             <div id="links">
                 <h1>Lets get in touch!</h1>
-                <a href="https://github.com/ScottDonnan">Github</a>
-                <a href="https://www.linkedin.com/in/scott-donnan">LinkedIn</a>
-                <span>Scott.Donnan@me.com</span>
-                <span>(512) 567-7434</span>
+                <div>
+                    <div id="prof-links">
+                        <a href="https://github.com/ScottDonnan">Github</a>
+                        <a href="https://www.linkedin.com/in/scott-donnan">LinkedIn</a>
+                    </div>
+                    <div id="contact">
+                        <p>Scott.Donnan@me.com</p>
+                        <p>(512) 567-7434</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
