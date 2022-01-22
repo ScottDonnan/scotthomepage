759215f09fb5d5a013789a061d87252b48fc6bb9
Scott Donnan <scott.donnan@me.com>
2022-01-21 22:03:08 -0600

    updating connect section

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,14 +8,20 @@ function Links() {
         <div ref={links} className="main-section">
             <div id="links">
                 <h1>Lets get in touch!</h1>
-                <div>
+                <p>Scott.Donnan@me.com</p>
+                <div id="link-forms">
+                    <form>
+                        <label for="name">Name</label><br/>
+                        <input type="text" name="name"/><br/>
+                        <label for="email">Email</label><br/>
+                        <input type="text" name="email" /><br/>
+                        <lable for="content">Content</lable><br/>
+                        <textarea rows="3" cols="20" id="content" name="content"/><br/>
+                        <input type="submit"/> 
+                    </form>
                     <div id="prof-links">
                         <a href="https://github.com/ScottDonnan">Github</a>
                         <a href="https://www.linkedin.com/in/scott-donnan">LinkedIn</a>
-                    </div>
-                    <div id="contact">
-                        <p>Scott.Donnan@me.com</p>
-                        <p>(512) 567-7434</p>
                     </div>
                 </div>
             </div>
