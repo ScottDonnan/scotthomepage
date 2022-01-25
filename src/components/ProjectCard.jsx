3d9d9985b93b547407f0da4545a693744be66b38
Scott Donnan <scott.donnan@me.com>
2022-01-25 12:25:40 -0600

    size updates

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,14 +4,14 @@ function ProjectCard({title, link, demoLink, githubLink, githubBackendLink, demo
     
     let media;
 
-    screenWidth >= 640 ? media = <iframe id="large-frame" title="largeFrame" width="640" height="480" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" src={demoLink}/> : media = <iframe id="small-frame" title="smallFrame" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" src={demoLink}/>
+    screenWidth >= 640 ? media = <iframe id="large-frame" title="largeFrame" width="640" height="480" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" src={demoLink}/> : media = <iframe id="small-frame" title="smallFrame" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" src={demoLink}/>
 
     return(
         <div className="project-card">
             <a href={link}>{title}</a>
             <a href={githubLink}>Github</a>
             { githubBackendLink ? <a href={githubBackendLink}>Backend</a> : null }
-            {demoLink ? media : <img src={siteThumb} alt="d&d website thumbnail"/>}
+            {demoLink ? media : <img src={siteThumb} alt="website thumbnail"/>}
             {/* { screenWidth >= 640 ? <iframe id="large-frame" width="640" height="480" src={demoLink}/> : <iframe id="small-frame" src={demoLink}/> } */}
             {/* {demoLink ? <iframe width="640" height="480" src={demoLink}/> : null } */}
             <Link to="/">ALL PROJECTS</Link>
